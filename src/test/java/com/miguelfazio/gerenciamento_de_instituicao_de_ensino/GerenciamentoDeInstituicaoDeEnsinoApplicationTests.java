0687a95683fdfb4f9c87ca7fcb98540531edbce5
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-01-19 19:17:22 -0300

    Estrutura inicial

---
+++ src/test/java/com/miguelfazio/gerenciamento_de_instituicao_de_ensino/GerenciamentoDeInstituicaoDeEnsinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguelfazio.gerenciamento_de_instituicao_de_ensino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeInstituicaoDeEnsinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
