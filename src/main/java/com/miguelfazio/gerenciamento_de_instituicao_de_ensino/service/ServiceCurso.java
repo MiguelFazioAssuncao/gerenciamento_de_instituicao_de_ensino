fde2998ecefb67a593d7306887b9475befa93f13
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-01-21 19:55:28 -0300

    Adicionado: - Método buscarPorId - Método realizarMatricula

--- src/main/java/com/miguelfazio/gerenciamento_de_instituicao_de_ensino/service/ServiceCurso.java
+++ src/main/java/com/miguelfazio/gerenciamento_de_instituicao_de_ensino/service/ServiceCurso.java
@@ -1,5 +1,6 @@
 package com.miguelfazio.gerenciamento_de_instituicao_de_ensino.service;
 
+import com.miguelfazio.gerenciamento_de_instituicao_de_ensino.model.Aluno;
 import com.miguelfazio.gerenciamento_de_instituicao_de_ensino.model.Curso;
 
 import java.util.List;
@@ -14,5 +15,21 @@ public class ServiceCurso {
 
     public List<Curso> consultarCursos() {
         return Curso.getCursos();
+    }
+
+    public static Curso buscarPoriD(int id) {
+        Curso curso = Curso.buscarPoriD(id);
+        if (curso == null) {
+            throw new IllegalArgumentException("Curso com ID " + id + " não encontrado.");
+        }
+        return curso;
+    }
+
+    public void realizarMatricula(int idCurso, Aluno aluno) {
+        Curso curso = buscarPoriD(idCurso);
+        if (aluno == null) {
+            throw new IllegalArgumentException("Aluno inválido. Matrícula não pode ser realizada.");
+        }
+        curso.getAlunosMatriculados().add(aluno);
     }
 }
